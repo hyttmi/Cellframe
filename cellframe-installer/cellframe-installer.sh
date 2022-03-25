887cb8a8f63c3de7879ee2bec947171eaa75c8f5
Mika Hyttinen <mika.hyttinen@gmail.com>
2022-03-25 18:36:27 +0200

    Playing with this

--- cellframe-installer/cellframe-installer.sh
+++ cellframe-installer/cellframe-installer.sh
@@ -7,6 +7,13 @@ DEPS="wget gnupg libpython3.9 libmagic1"
 ARCH=`uname -m`
 CODENAME=`lsb_release -c | cut -f2`
 
+if  $(which lsb_release > /dev/null) ; then
+    echo "lsb_release installed. Continuing..."
+else
+    echo "lsb_release not found. You can install it with apt-get install lsb-release"
+    exit 1
+fi
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root." 
    exit 1
