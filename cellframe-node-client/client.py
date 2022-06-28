27d20b43943b339832c405360836cabc379b2fb8
Mika Hyttinen <mika.hyttinen@gmail.com>
2022-06-28 20:22:29 +0300

    Mimic cellframe-node-cli

--- cellframe-node-client/client.py
+++ cellframe-node-client/client.py
@@ -9,19 +9,23 @@ print("Creating socket...")
 try:
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 except socket.error:
-    print('Failed to create socket')
+    print("Failed to create socket")
     sys.exit()
 print("Connecting to server, " + socket_path)
 s.connect(socket_path)
 
-print(f'Post help to {socket_path}')
+command = ""
+while command != "exit":
+    command = input("> ")
+    length = str(len(command))
+    post = "POST /connect HTTP/1.1\r\nHost: localhost\r\nContent-Type: text/text\r\nContent-Length: " + length + "\r\n\r\n" + command + "\r\n\r\n"
+    post = bytes(post,"utf-8")
 
-cmd = "help"
-length = str(len(cmd))
-post = "POST /connect HTTP/1.1\r\nHost: localhost\r\nContent-Type: text/text\r\nContent-Length: " + length + "\r\n\r\n" + cmd + "\r\n\r\n"
-post = bytes(post,"utf-8")
-
-s.sendall(post)
-print('# Receive data from server')
-reply = s.recv(4096)
-print (reply.decode("utf-8"))
+    s.sendall(post)
+    reply = s.recv(4096)
+    reply = reply.decode("utf-8")
+    reply = reply.split("\n")
+    reply = list(filter(None, reply))
+    reply = reply[4:]
+    for replies in reply:
+        print(replies)
