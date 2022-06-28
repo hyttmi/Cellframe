da1e14cf58d83bea67c77c509177f1826a4c0834
Mika Hyttinen <mika.hyttinen@gmail.com>
2022-06-28 07:38:06 +0300

    Add sample code for interacting with cellframe-node with Python and sockets

---
+++ cellframe-node-client/client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from calendar import c
+import socket
+import sys
+import time 
+
+socket_path = "/opt/cellframe-node/var/run/node_cli"
+
+print("Creating socket...")
+
+try:
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+except socket.error:
+    print('Failed to create socket')
+    sys.exit()
+print("Connecting to server, " + socket_path)
+s.connect(socket_path)
+
+print(f'Post help to {socket_path}')
+
+cmd = "help"
+length = str(len(cmd))
+post = "POST /connect HTTP/1.1\r\nHost: localhost\r\nContent-Type: text/text\r\nContent-Length: " + length + "\r\n\r\n" + cmd + "\r\n\r\n"
+post = bytes(post,"utf-8")
+
+s.sendall(post)
+print('# Receive data from server')
+reply = s.recv(4096)
+print (reply.decode("utf-8"))
