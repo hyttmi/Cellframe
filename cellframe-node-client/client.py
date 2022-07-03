a3cbc078545ac45d97de463657fc3d533977d062
Mika Hyttinen <mika.hyttinen@gmail.com>
2022-07-03 21:01:47 +0300

    Just make it work

--- cellframe-node-client/client.py
+++ cellframe-node-client/client.py
@@ -4,23 +4,16 @@ import sys
 
 socket_path = "/opt/cellframe-node/var/run/node_cli"
 
-print("Creating socket...")
-
-try:
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-except socket.error:
-    print("Failed to create socket")
-    sys.exit()
-print("Connecting to server, " + socket_path)
-s.connect(socket_path)
-
-command = input("> ")
-if command != "exit":    
+while True:
+    command = input("> ")
+    if command == "exit":
+        break
     length = str(len(command))
     command = command.replace(" ", "\r\n")
     post = "POST /connect HTTP/1.1\r\nHost: localhost\r\nContent-Type: text/text\r\nContent-Length: " + length + "\r\n\r\n" + command + "\r\n\r\n"
     post = bytes(post,"utf-8")
-
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    s.connect(socket_path)
     s.sendall(post)
     reply = s.recv(4096)
     reply = reply.decode("utf-8")
@@ -31,4 +24,4 @@ if command != "exit":
         print(replies)
     s.shutdown(socket.SHUT_RDWR)
     s.close()
-    sys.exit() # Exit this client because Node has a bug somewhere in socket connection and sending multiple commands won't work.
+    # Exit this client because Node has a bug somewhere in socket connection and sending multiple commands won't work.
