83da82376975786cfe15d42f88a97840f69a7b47
Mika Hyttinen <mika.hyttinen@gmail.com>
2022-07-20 23:34:26 +0300

    Maybe works now?

--- cellframe-plugins/redirectoooooor/redirectoooooor.py
+++ cellframe-plugins/redirectoooooor/redirectoooooor.py
@@ -11,36 +11,29 @@ port = 12345
 def redirectData():
     node_socket_path = configGetItem("conserver", "listen_unix_socket_path")
 
-    local_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
     ext_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    try:
-        local_socket.connect(node_socket_path)
-        logIt.notice(f"{plugin_name}: Connected to local socket!") 
-    except:
-        logIt.error(f"{plugin_name}: Error connecting to local socket!") 
+    ext_socket.bind(("localhost", port))
 
-    ext_conn = False
-    try:
-        ext_socket.bind(("127.0.0.1", port))
-        logIt.notice(f"{plugin_name}: Socket created!") 
-        ext_conn = True
-    except:
-        logIt.error(f"{plugin_name}: Error creating socket!") 
-
-    if ext_conn:
+    while True:
+        local_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         ext_socket.listen()
         logIt.notice(f"{plugin_name}: External socket listening...")
-        conn, addr = ext_socket.accept()
-        logIt.notice(f"{plugin_name}: Client connected!")
-        with conn:
-            while True:
-                fwd = conn.recv(1024)
-                local_socket.sendall(fwd)
-                data = local_socket.recv(1024)
-                conn.sendto(data, addr)
-                conn.close()
+        ext_conn, addr = ext_socket.accept()
+        logIt.notice(f"{plugin_name}: Client connected from {addr}!")
+        fwd = ext_conn.recv(1024)
+        try:
+            local_socket.connect(node_socket_path)
+            logIt.notice(f"{plugin_name}: Connected to local socket!")
+        except:
+            logIt.error(f"{plugin_name}: Connection to local socket failed!")
+        fwd_str = fwd.decode("utf-8")
+        logIt.notice(f"{plugin_name}: Received data: {fwd_str}")
+        local_socket.sendall(fwd)
+        data = local_socket.recv(1024)
+        ext_conn.sendto(data, addr)
+        ext_conn.close()
+        local_socket.close()
 
 def init():
     logIt.notice(f"{plugin_name} version {version} started...")
