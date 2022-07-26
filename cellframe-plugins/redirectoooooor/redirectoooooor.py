0a46d3b395dd8c7ca384de81380a3c764865c9a1
Mika Hyttinen <mika.hyttinen@gmail.com>
2022-07-26 07:28:13 +0300

    Remove writeLog, it's not needed

--- cellframe-plugins/redirectoooooor/redirectoooooor.py
+++ cellframe-plugins/redirectoooooor/redirectoooooor.py
@@ -11,16 +11,6 @@ version="0.1"
 port = 12345
 allowed = ["127.0.0.1", "localhost", "192.168.1.10"] # use external IP address if connecting remotely!
 
-def writeLog(address, command):
-    filepath = os.path.abspath(os.path.dirname(__file__))
-    logfile = filepath + "/" + plugin_name + ".log"
-    timestamp = datetime.datetime.now()
-    timestamp = timestamp.strftime("%d-%m-%Y %H:%M:%S")
-
-    f = open(logfile, "a", encoding="utf-8")
-    f.write(f"{timestamp} {address} {command}\n")
-    f.close()
-
 def redirectData():
     node_socket_path = configGetItem("conserver", "listen_unix_socket_path")
 
@@ -29,13 +19,12 @@ def redirectData():
     try:
         ext_socket.bind(("0.0.0.0", port))
         ext_socket.listen()
+        logIt.notice(f"{plugin_name}: External socket listening...")
     except:
         logIt.error("Failed to bind external socket! Port in use?")
 
     while True:
         local_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        
-        logIt.notice(f"{plugin_name}: External socket listening...")
         ext_conn, addr = ext_socket.accept()
         logIt.notice(f"{plugin_name}: Client connected from {addr}!")
         if addr[0] not in allowed:
@@ -46,7 +35,6 @@ def redirectData():
         fwd_str = fwd.decode("utf-8")
         fwd_str = fwd_str.replace("\r\n", " ")
         logIt.notice(f"{plugin_name}: Received data: {fwd_str}")
-        writeLog(addr, fwd_str)
         try:
             local_socket.connect(node_socket_path)
             logIt.notice(f"{plugin_name}: Connected to local socket!")
