a6b4f367c01922388e8b93d91b2bca97b73993e6
Mika Hyttinen <mika.hyttinen@gmail.com>
2022-07-29 18:52:55 +0300

    Add some logging to see what's happening

--- cellframe-plugins/webui/webui.py
+++ cellframe-plugins/webui/webui.py
@@ -4,14 +4,14 @@ from functions import *
 
 from DAP.Core import logIt
 
+plugin_name = "Cellframe node WebUI"
 port = 8000
 filepath = os.path.abspath(os.path.dirname(__file__))
-
 
 class WebUIHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=filepath, **kwargs)
-
+        
     def do_POST(self):
         length = int(self.headers.get_all('content-length')[0])
         self.send_response(200)
@@ -19,12 +19,13 @@ class WebUIHandler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         data_string = self.rfile.read(length)
         data_string = data_string.decode("utf-8")
+        logIt.message(f"({plugin_name}) Received data: {data_string}")
         data = sendCommand(data_string)
         self.wfile.write(data.encode("utf-8"))
 
 
 def init():
-    logIt.message("WebUI serving at port " + str(port))
+    logIt.message(f"({plugin_name}) WebUI serving at port {str(port)}")
     server_address = ("0.0.0.0", port)
     server = http.server.ThreadingHTTPServer(server_address, WebUIHandler)
     server.serve_forever()
