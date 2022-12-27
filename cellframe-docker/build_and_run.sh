d495d21b42f33ac1fbb430497f5ddd54969a3506
Mika Hyttinen <mika.hyttinen@gmail.com>
2022-12-27 09:06:18 +0200

    Better approach

--- cellframe-docker/build_and_run.sh
+++ cellframe-docker/build_and_run.sh
@@ -2,17 +2,18 @@
 
 if [[ $# -lt 1 ]] ; then
     echo "Usage: ./build_and_run.sh <name> [-d] (-d for daemonizing)"
+    exit 1
 
 elif [[ $# -eq 2 ]]; then
+    
+    if [[ "$2" != "-d" ]] ; then
+            echo "Unsupported argument: $2, exiting..."
+            exit 1
+    else
         docker build -t "$1" .
         docker volume create "$1"
-
-        if [[ "$2" != "-d" ]] ; then
-            echo "Unsupported argument: $2, not daemonizing node..."
-            docker run -v "$1":/home/cellframe --name="$1" --privileged --net=host -it "$1"
-        else
-            docker run -v "$1":/home/cellframe --name="$1" --privileged --net=host -it -d "$1"
-        fi
+        docker run -v "$1":/home/cellframe --name="$1" --privileged --net=host -it -d "$1" 
+    fi
 
 else
     docker build -t "$1" .
