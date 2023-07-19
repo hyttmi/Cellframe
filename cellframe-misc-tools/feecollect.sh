bd95087d4e523a59221e3350e2854bdfe6cd99a2
Mika Hyttinen <mika.hyttinen@gmail.com>
2023-07-19 07:35:03 +0300

    ...

--- cellframe-misc-tools/feecollect.sh
+++ cellframe-misc-tools/feecollect.sh
@@ -2,7 +2,13 @@
 
 CERT="<your_certificate_name>"
 WALLET_ADDR="<your_wallet_address>"
-HASHES=$(sh -c "/opt/cellframe-node/bin/cellframe-node-cli block list -net Backbone -chain main -cert backbone.mikah -unspent | grep -oP '0x.{64}' | tr '\n' ','")
+HASHES=$(sh -c "/opt/cellframe-node/bin/cellframe-node-cli block list -net Backbone -chain main -cert $CERT -unspent | grep -oP '0x.{64}' | tr '\n' ','")
 
-sh -c "/opt/cellframe-node/bin/cellframe-node-cli block fee collect -cert $CERT -addr $WALLET_ADDR -net Backbone -chain main -hashes $HASHES -fee 0.05e+18
+
+if [[ -z $HASHES ]]; then
+    echo "Can't get any hashes, is cellframe-node running?"
+else
+    sh -c "/opt/cellframe-node/bin/cellframe-node-cli block fee collect -cert $CERT -addr $WALLET_ADDR -net Backbone -chain main -hashes $HASHES -fee 0.05e+18"
+fi
+
 
